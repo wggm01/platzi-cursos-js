b8a105993fb484a14ce24164e413202beee0ed05
Wvaldo Graell <wvaldo.graell@utp.ac.pa>
2023-01-08 12:16:10 -0500

    ultimos cambios

---
+++ script.js
@@ -0,0 +1,5 @@
+const title = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
